dbb4323492a6efa684347af5a1a1f17a545801fb
Adam Stitou <adamkoora27@gmail.com>
2024-03-20 01:48:33 +0000

    Adding the use client directive that i completely forget about

--- app/update-idea/page.jsx
+++ app/update-idea/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router"; // Import useRouter instead of next/navigation
 import Form from "@components/Form";
